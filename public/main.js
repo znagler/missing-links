7f78d444880c9866699a5408a4f9a83baf0fa4a2
Zack Nagler <zachary.nagler@gmail.com>
2015-01-08 15:20:02 -0500

    make both ajax calls fire on any keyup

--- public/main.js
+++ public/main.js
@@ -1,40 +1,77 @@
 $( document ).ready(function() {
 
-	$('.start-input').keyup(function() {
+	$('input').keyup(function(){
 		delay(function(){
 
-		if ($('.start-input').val() != ""){
-				$.ajax({
-					url: '/start',
-					type: 'POST',
-					data: {start_wiki: $('.start-input').val()},
-				})
-				.done(function(response) {
-					$('.show-start').text(response)
-				})
+			if ($('.start-input').val() != ""){
+					$.ajax({
+						url: '/start',
+						type: 'POST',
+						data: {start_wiki: $('.start-input').val()},
+					})
+					.done(function(response) {
+						$('.show-start').text(response)
+						setButtonColorAndMessage()
+					})
+					}
+
+								if ($('.finish-input').val() != ""){
+					$.ajax({
+						url: '/finish',
+						type: 'POST',
+						data: {finish_wiki: $('.finish-input').val()},
+					})
+					.done(function(response) {
+						$('.show-finish').text(response)
+						setButtonColorAndMessage()
+					})
 				}
-			}, 300 )
+				}, 150 )
 		})
-
-
-	$('.finish-input').keyup(function() {
-
-		delay(function(){
-
-		if ($('.finish-input').val() != ""){
-				$.ajax({
-					url: '/finish',
-					type: 'POST',
-					data: {finish_wiki: $('.finish-input').val()},
-				})
-				.done(function(response) {
-					$('.show-finish').text(response)
-				})
-				}
-			}, 300 )
-		})
-
 })
+
+
+
+
+
+
+	// $('.start-input').keyup(function() {
+	// 	delay(function(){
+
+	// 	if ($('.start-input').val() != ""){
+	// 			$.ajax({
+	// 				url: '/start',
+	// 				type: 'POST',
+	// 				data: {start_wiki: $('.start-input').val()},
+	// 			})
+	// 			.done(function(response) {
+	// 				$('.show-start').text(response)
+	// 				setButtonColor()
+	// 			})
+	// 			}
+	// 		}, 300 )
+	// 	})
+
+
+	// $('.finish-input').keyup(function() {
+
+	// 	delay(function(){
+
+	// 	if ($('.finish-input').val() != ""){
+	// 			$.ajax({
+	// 				url: '/finish',
+	// 				type: 'POST',
+	// 				data: {finish_wiki: $('.finish-input').val()},
+	// 			})
+	// 			.done(function(response) {
+	// 				$('.show-finish').text(response)
+	// 				setButtonColor()
+	// 			})
+	// 			}
+	// 		}, 300 )
+// 	// 	})
+
+// })
 
 var delay = (function(){
 	var timer = 0;
@@ -43,3 +80,13 @@ var delay = (function(){
 		timer = setTimeout(callback, ms);
 	};
 })();
+
+function setButtonColorAndMessage(){
+		if ($(".show-start").text()  != "" && $(".show-finish").text()  != ""){
+		$("button").addClass('button-red')
+		$("button").text("GO")
+		} else{
+			$("button").removeClass('button-red')
+		}
+
+}
