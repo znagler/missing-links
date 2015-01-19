f4ef7729b2c861d7a021eb729e695a8eb3363c3f
Zack Nagler <zachary.nagler@gmail.com>
2015-01-19 15:05:24 -0500

    decrease output object size for heroku test

--- cool.js
+++ cool.js
@@ -41,8 +41,8 @@ module.exports = {
 							}
 						})
 						var uniqueHrefArray = arrayUnique(linkDataArray)
-						if (uniqueHrefArray.length > 500) {
-							callback(getRandomSubarray(uniqueHrefArray, 500))
+						if (uniqueHrefArray.length > 200) {
+							callback(getRandomSubarray(uniqueHrefArray, 200))
 						} else {
 							callback(uniqueHrefArray)
 						}
@@ -67,8 +67,8 @@ module.exports = {
 
 						})
 						var uniqueHrefArray = arrayUnique(hrefArray)
-						if (uniqueHrefArray.length > 500) {
-							callback(getRandomSubarray(uniqueHrefArray, 500))
+						if (uniqueHrefArray.length > 200) {
+							callback(getRandomSubarray(uniqueHrefArray, 200))
 						} else {
 							callback(uniqueHrefArray)
 						}
